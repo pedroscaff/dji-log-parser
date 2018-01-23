bf675643f5c9d603c1f3ca18753a050e435ec3eb
Pedro Scaff <ext-pedro.scaff@here.com>
2018-01-23 11:43:37 +0100

    Return promise on log2json; save to file or return the object;

--- log2json.js
+++ log2json.js
@@ -1,17 +1,26 @@
-module.exports = function(logfile, output) {
-	let DJIParser = require("./lib/index");
-	let fs = require("fs");
-	// let argv = require("minimist")(process.argv.slice(2));
-	let FramesCentral = require("./lib/frames-central");
+module.exports = function(logfile, toFile, outputPath) {
+	return new Promise((resolve, reject) => {
+		if (toFile && !outputPath) {
+			reject("no file path provided!");
+		}
+		let DJIParser = require("./lib/index");
+		let fs = require("fs");
+		let FramesCentral = require("./lib/frames-central");
 
-	let parser = new DJIParser();
+		let parser = new DJIParser();
 
-	let framesCentral = new FramesCentral(parser);
+		let framesCentral = new FramesCentral(parser);
 
-	// details is the last frame
-	parser.on("DETAILS", () => {
-		framesCentral.saveToJSON(output);
+		// details is the last frame
+		parser.on("DETAILS", () => {
+			if (toFile) {
+				framesCentral.saveToJSON(outputPath);
+				resolve(`file saved to ${outputPath}`);
+			} else {
+				resolve(framesCentral.getEntries());
+			}
+		});
+
+		parser.parse(fs.readFileSync(logfile));
 	});
-
-	parser.parse(fs.readFileSync(logfile));
 }
